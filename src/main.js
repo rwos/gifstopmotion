7d216a59a76c499e6800eda0b32e4533dc82bd0f
Richard Wossal <richard@r-wos.org>
2015-07-12 19:59:07 +0200

    changed chroma key

--- src/main.js
+++ src/main.js
@@ -22,6 +22,7 @@
     var lastframe = null;
     var preview = null;
     var text = null;
+    var debug = null;
     var framecontainer = null;
     var greenscreen = null;
 
@@ -31,7 +32,7 @@
         quality: 10,
         workers: 2,
         workerScript: URL.createObjectURL(workerblob),
-        transparent: 0x000000,
+        transparent: 0xff00ff,
     });
 
     var delay = 200;
@@ -42,6 +43,7 @@
         lastframe = document.getElementById('lastframe');
         preview = document.getElementById('preview');
         text = document.getElementById('text');
+        debug = document.getElementById('debug');
         framecontainer = document.getElementById('framecontainer');
         document.getElementById('clear').addEventListener('click', function(ev) {
             clear();
@@ -101,6 +103,10 @@
                 ev.preventDefault();
             }
         }, false);
+
+        if (document.location.hash === '#d') {
+            debug.className = '';    
+        }
     }
 
     function clonecanvas(oldcanvas) {
@@ -141,7 +147,15 @@
         }
     }
 
+    function debugcapture(canvas) {
+        var img = document.createElement('img');
+        img.setAttribute('src', canvas.toDataURL('image/png'));
+        img.setAttribute('class', 'frame');
+        debug.appendChild(img);
+    }
+
     function addframe() {
+        /// XXX move all this document stuff into init
         document.getElementById('save').className = ''; // enable button
 
         var context = canvas.getContext('2d');
@@ -153,20 +167,10 @@
             context.drawImage(video, 0, 0, width, height);
             // remove background
             if (greenscreen) {
-                /// XXX DEBUG
-                var img = document.createElement('img');
-                img.setAttribute('src', greenscreen.toDataURL('image/png'));
-                img.setAttribute('class', 'frame');
-                document.body.appendChild(img);
-                /// XXX
+                debugcapture(greenscreen);
                 context.globalCompositeOperation = 'difference'; /// XXX this is too simplistic
                 context.drawImage(greenscreen, 0, 0, width, height);
-                /// XXX DEBUG
-                var img = document.createElement('img');
-                img.setAttribute('src', canvas.toDataURL('image/png'));
-                img.setAttribute('class', 'frame');
-                document.body.appendChild(img);
-                /// XXX
+                debugcapture(canvas);
                 context.globalCompositeOperation = 'source-over';
                 var map = context.getImageData(0, 0, width, height);
                 var l = map.data.length / 4;
@@ -181,19 +185,14 @@
                         map.data[i * 4 + 2] = 255;
                         map.data[i * 4 + 3] = 255;
                     } else {
-                        map.data[i * 4 + 0] = 0;
+                        map.data[i * 4 + 0] = 255;
                         map.data[i * 4 + 1] = 0;
-                        map.data[i * 4 + 2] = 0;
+                        map.data[i * 4 + 2] = 255;
                         map.data[i * 4 + 3] = 0;
                     }
                 }
                 context.putImageData(map, 0, 0);
-                /// XXX DEBUG
-                var img = document.createElement('img');
-                img.setAttribute('src', canvas.toDataURL('image/png'));
-                img.setAttribute('class', 'frame');
-                document.body.appendChild(img);
-                /// XXX
+                debugcapture(canvas);
                 context.globalCompositeOperation = 'source-in';
                 context.drawImage(video, 0, 0, width, height);
                 context.globalCompositeOperation = 'source-over';
@@ -207,6 +206,7 @@
             context.strokeText(text.value, width/2, height-24);
 
             // update transparent last frame overlay
+            lastframe.getContext('2d').clearRect(0, 0, width, height);
             lastframe.getContext('2d').drawImage(canvas, 0, 0, width, height);
 
             // add frame to frame list
@@ -220,6 +220,14 @@
             });
 
             // update gif
+            if (greenscreen) {
+                context.globalCompositeOperation = 'destination-over';
+                /// XXX: this must match the gifjs "transparent" value
+                context.fillStyle = "rgb(255,0,255)"
+                context.fillRect(0, 0, width, height);
+                debugcapture(canvas);
+                context.globalCompositeOperation = 'source-over';
+            }
             gif.addFrame(clonecanvas(canvas), {delay: delay});
             gif.on('finished', function(blob) {
                 preview.removeAttribute('height');
