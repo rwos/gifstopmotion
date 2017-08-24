3c1194b3606483d28311618d5af0748a80d3a22f
Richard Wossal <richard@r-wos.org>
2017-08-24 19:43:02 +0200

    some JS cleanup

--- src/main.js
+++ src/main.js
@@ -85,6 +85,7 @@
     var height = 0;
     var streaming = false;
 
+    var ELEMENT = {};
     var video = null;
     var canvas = null;
     var lastframe = null;
@@ -97,15 +98,19 @@
     var workerblob = new Blob([document.getElementById('workerscript').textContent], {type: 'text/javascript'});
 
     function init() {
-        video = document.getElementById('viewfinder');
-        canvas = document.getElementById('tmp');
-        lastframe = document.getElementById('lastframe');
-        preview = document.getElementById('preview');
-        text = document.getElementById('text');
-        debug = document.getElementById('debug');
-        framecontainer = document.getElementById('framecontainer');
-        document.getElementById('clear').addEventListener('click', clear);
+        ELEMENT = {
+            video: document.getElementById('viewfinder'),
+            canvas: document.getElementById('tmp'),
+            lastframe: document.getElementById('lastframe'),
+            preview: document.getElementById('preview'),
+            text: document.getElementById('text'),
+            debug: document.getElementById('debug'),
+            framecontainer: document.getElementById('framecontainer'),
+            clear: document.getElementById('clear'),
+        };
+        ELEMENT.clear.addEventListener('click', clear);
 
+        /// XXX integrate this stuff, too
         document.getElementById('greenscreen').addEventListener('click', function(ev) {
             updategreenscreen();
             document.getElementById('greenscreen').className = 'disabled';
@@ -123,26 +128,26 @@
                            || navigator.msGetUserMedia);
         navigator.getMedia({video: true, audio: false}, function(stream) {
             if (navigator.mozGetUserMedia) {
-                video.mozSrcObject = stream;
+                ELEMENT.video.mozSrcObject = stream;
             } else {
                 var vendorURL = window.URL || window.webkitURL;
-                video.src = vendorURL.createObjectURL(stream);
+                ELEMENT.video.src = vendorURL.createObjectURL(stream);
             }
-            video.play();
+            ELEMENT.video.play();
         }, console.log);
 
-        video.addEventListener('canplay', function(ev) {
+        ELEMENT.video.addEventListener('canplay', function(ev) {
             if (streaming) {
                 return;
             }
-            height = video.videoHeight / (video.videoWidth/width);
+            height = ELEMENT.video.videoHeight / (ELEMENT.video.videoWidth/width);
             if (isNaN(height)) { // FF bug, apparently
                 height = width / (4/3);
             }
-            canvas.setAttribute('width', width);
-            canvas.setAttribute('height', height);
-            lastframe.setAttribute('width', width);
-            lastframe.setAttribute('height', height);
+            ELEMENT.canvas.setAttribute('width', width);
+            ELEMENT.canvas.setAttribute('height', height);
+            ELEMENT.lastframe.setAttribute('width', width);
+            ELEMENT.lastframe.setAttribute('height', height);
             streaming = true;
         }, false);
 
@@ -203,19 +208,19 @@
     function download() {
         var a = document.createElement('a');
         a.setAttribute('download', 'gif.gif');
-        a.href = preview.src;
+        a.href = ELEMENT.preview.src;
         a.style.display = 'none';
         document.body.appendChild(a);
         a.click();
     }
 
     function updategreenscreen() {
-        var context = canvas.getContext('2d');
+        var context = ELEMENT.canvas.getContext('2d');
         if (width && height) {
-            canvas.width = width;
-            canvas.height = height;
-            context.drawImage(video, 0, 0, width, height);
-            greenscreen = clonecanvas(canvas);
+            ELEMENT.canvas.width = width;
+            ELEMENT.canvas.height = height;
+            context.drawImage(ELEMENT.video, 0, 0, width, height);
+            greenscreen = clonecanvas(ELEMENT.canvas);
         }
     }
 
@@ -227,23 +232,23 @@
         var img = document.createElement('img');
         img.setAttribute('src', canvas.toDataURL('image/png'));
         img.setAttribute('class', 'frame');
-        debug.appendChild(img);
+        ELEMENT.debug.appendChild(img);
     }
 
     function addframe() {
-        var context = canvas.getContext('2d');
+        var context = ELEMENT.canvas.getContext('2d');
         if (width && height) {
-            canvas.width = width;
-            canvas.height = height;
+            ELEMENT.canvas.width = width;
+            ELEMENT.canvas.height = height;
 
             // prepare frame
-            context.drawImage(video, 0, 0, width, height);
+            context.drawImage(ELEMENT.video, 0, 0, width, height);
             // remove background
             if (store.getState().settings.greenscreen) {
                 debugcapture(greenscreen);
                 context.globalCompositeOperation = 'difference'; /// XXX this is too simplistic
                 context.drawImage(greenscreen, 0, 0, width, height);
-                debugcapture(canvas);
+                debugcapture(ELEMENT.canvas);
                 context.globalCompositeOperation = 'source-over';
                 var map = context.getImageData(0, 0, width, height);
                 var l = map.data.length / 4;
@@ -265,9 +270,9 @@
                     }
                 }
                 context.putImageData(map, 0, 0);
-                debugcapture(canvas);
+                debugcapture(ELEMENT.canvas);
                 context.globalCompositeOperation = 'source-in';
-                context.drawImage(video, 0, 0, width, height);
+                context.drawImage(ELEMENT.video, 0, 0, width, height);
                 context.globalCompositeOperation = 'source-over';
             }
             // add caption
@@ -275,14 +280,14 @@
             context.font = 'bold 24px sans';
             context.fillStyle = 'white';
             context.strokeStyle = '1px solid black';
-            context.fillText(text.value, width/2, height-24);
-            context.strokeText(text.value, width/2, height-24);
+            context.fillText(ELEMENT.text.value, width/2, height-24);
+            context.strokeText(ELEMENT.text.value, width/2, height-24);
 
             // update transparent last frame overlay
             store.dispatch({
                 type: 'FRAME_ADD',
-                canvas: clonecanvas(canvas),
-                frame: canvas.toDataURL('image/png')
+                canvas: clonecanvas(ELEMENT.canvas),
+                frame: ELEMENT.canvas.toDataURL('image/png')
             });
 
             if (greenscreen) {
@@ -290,7 +295,7 @@
                 /// XXX: this must match the gifjs "transparent" value
                 context.fillStyle = "rgb(255,0,255)";
                 context.fillRect(0, 0, width, height);
-                debugcapture(canvas);
+                debugcapture(ELEMENT.canvas);
                 context.globalCompositeOperation = 'source-over';
             }
         }
@@ -358,7 +363,7 @@
             frameId = frames[frameCounter].id;
 
             if (typeof domFramesMap[frameId] === "undefined") {
-                buildPreviewFrame(framecontainer, frameId, frame, frameLength-1 === frameCounter);
+                buildPreviewFrame(ELEMENT.framecontainer, frameId, frame, frameLength-1 === frameCounter);
             }
             gif.addFrame(frameCanvas, {delay: delay});
             delete domFramesMap[frameId];
@@ -370,30 +375,30 @@
         }
 
         if (frames.length) {
-            lastframe.getContext('2d').clearRect(0, 0, width, height);
-            lastframe.getContext('2d').drawImage(frameCanvas, 0, 0, width, height);
+            ELEMENT.lastframe.getContext('2d').clearRect(0, 0, width, height);
+            ELEMENT.lastframe.getContext('2d').drawImage(frameCanvas, 0, 0, width, height);
 
             gif.render();
             gif.on('finished', function(blob) {
-                preview.removeAttribute('height');
-                preview.src = '';
-                preview.src = URL.createObjectURL(blob);
+                ELEMENT.preview.removeAttribute('height');
+                ELEMENT.preview.src = '';
+                ELEMENT.preview.src = URL.createObjectURL(blob);
                 gif.abort();
             });
 
             document.getElementById('save').className = '';
-            framecontainer.className = '';
+            ELEMENT.framecontainer.className = '';
         } else {
-            lastframe.getContext('2d').clearRect(0, 0, width, height);
+            ELEMENT.lastframe.getContext('2d').clearRect(0, 0, width, height);
 
-            preview.src = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
-            preview.setAttribute('height', height);
+            ELEMENT.preview.src = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
+            ELEMENT.preview.setAttribute('height', height);
             document.getElementById('save').className = 'disabled';
-            framecontainer.className = 'disabled';
+            ELEMENT.framecontainer.className = 'disabled';
         }
 
         if (state.settings.debug) {
-            debug.className = '';
+            ELEMENT.debug.className = '';
         }
     });
 
